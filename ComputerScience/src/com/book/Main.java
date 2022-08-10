179fbddfd636d0ef0c431f703742151168d92e5a
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2022-08-10 23:42:24 +0300

    add ex. 1.2.24 to CS project

--- ComputerScience/src/com/book/Main.java
+++ ComputerScience/src/com/book/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("The answer is: " + FirstChapter.randomGaussian());
-		System.out.println("Try to use github");
+		System.out.println("При размещени 1000$ под 25% на 10 лет, вы получите: " + (int)
+		FirstChapter.interestCapitalization(10, 1000, 25) + "$");
 
 	}
 }
