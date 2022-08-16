5a4baac32bb1752f94063219a69a767980ef929d
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2022-08-17 00:20:48 +0300

    Add harmonicNumber method
    Create com.book.algorithm.Examples class and add firstexample fromm CS book.

--- ComputerScience/src/com/book/Main.java
+++ ComputerScience/src/com/book/Main.java
@@ -1,11 +1,12 @@
 package com.book;
 
+import com.book.algorithm.Examples;
 import com.book.exercise.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Testing windCooling method ->  " + FirstChapter.windCooling(300, 5));
+		System.out.println("Testing harmonicNumber method ->  " + Examples.harmonicNumber(1000000));
 
 	}
 }
