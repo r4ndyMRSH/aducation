f30ca65c2482f1db4eab4491b931b6377b6de750
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2022-08-17 13:25:51 +0300

    sqrtNewton
    Add sqrtNewton(double c) method

--- ComputerScience/src/com/book/Main.java
+++ ComputerScience/src/com/book/Main.java
@@ -7,7 +7,7 @@ import static com.book.exercise.FirstChapter.randomGaussian;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Testing harmonicNumber method ->  " + Examples.harmonicNumber(1000000));
+		System.out.println("Testing sqrtNewton method ->  " + Examples.sqrtNewton(2));
 
 	}
 }
