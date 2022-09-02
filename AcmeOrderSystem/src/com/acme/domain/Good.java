a07707289505f35aa36103e2e1c8b65e30025cc3
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2022-09-02 23:03:15 +0300

    Lab #15
    Collections. Trying ArrayList & HashSet.

--- AcmeOrderSystem/src/com/acme/domain/Good.java
+++ AcmeOrderSystem/src/com/acme/domain/Good.java
@@ -1,17 +1,52 @@
 package com.acme.domain;
 
-public abstract class Good implements  Product {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Iterator;
+
+
+public abstract class Good implements Product {
 	public enum UnitOfMeasureType {
 		LITER, GALLON, CUBIC_METER, CUBIC_FEET
 	}
 
+	// Fields
 	private String name;
 	private int modelNumber;
 	private double height;
 	private UnitOfMeasureType unitOfMeasure;
 	private boolean flammable = true;
 	private double weightPerUofM;
+	private static Set catalog;
 
+	// Initialisation blocks
+	static {
+		Liquid glue = new Liquid("Acme Glue", 2334, 4, UnitOfMeasureType.LITER, false, 15, 6);
+		Liquid paint = new Liquid("Acme Invisible Paint", 2490, 0.65, UnitOfMeasureType.GALLON, true, 0.70, 12);
+		Solid anvil = new Solid("Acme Anvil", 1668, 0.3, UnitOfMeasureType.CUBIC_METER, false, 500, 0.25, 0.3);
+		Solid safe = new Solid("Acme Safe", 1672, 1.0, UnitOfMeasureType.CUBIC_METER, false, 300, 0.5, 0.5);
+		Solid balloon = new Solid("Acme Balloon", 1401, 15, UnitOfMeasureType.CUBIC_FEET, false, 10, 5, 5);
+		Solid pistol = new Solid("Acme Disintegrating Pistol", 1587, 0.1, UnitOfMeasureType.CUBIC_FEET, false, 1, 0.5,
+				2);
+		Liquid nitro = new Liquid("Acme Nitroglycerin", 4289, 1.0, UnitOfMeasureType.CUBIC_METER, true, 1.5, 0.25);
+		Liquid oil = new Liquid("Acme Oil", 4275, 1.0, UnitOfMeasureType.CUBIC_METER, true, 1.5, 0.25);
+
+		catalog = new HashSet();
+		catalog.add(glue);
+		catalog.add(paint);
+		catalog.add(anvil);
+		catalog.add(safe);
+		catalog.add(balloon);
+		catalog.add(pistol);
+		catalog.add(nitro);
+		catalog.add(oil);
+
+	}
+
+	// Constructors
 	public Good(String name, int modelNumber, double height, UnitOfMeasureType uoM, boolean flammable,
 			double wgtPerUoM) {
 		this.name = name;
@@ -22,6 +57,7 @@ public abstract class Good implements  Product {
 		this.weightPerUofM = wgtPerUoM;
 	}
 
+	@Override
 	public String toString() {
 		return name + "-" + modelNumber;
 	}
@@ -31,13 +67,27 @@ public abstract class Good implements  Product {
 	public double weight() {
 		return volume() * weightPerUofM;
 	}
-	
+
 	public final boolean canShipViaPostOffice() {
 		if (!(isFlammable()) && (getWeightPerUofM() < 200))
 			return true;
-		else return false;
+		else
+			return false;
 	}
 
+	public static Set flammablesList() {
+		Set flammables = new HashSet();
+		Iterator i = Good.getCatalog().iterator();
+		while (i.hasNext()) {
+			Good x = (Good) i.next();
+			if (x.isFlammable()) {
+				flammables.add(x);
+			}
+		}
+		return flammables;
+	}
+
+	// Getters & Setters
 	public String getName() {
 		return name;
 	}
@@ -84,6 +134,10 @@ public abstract class Good implements  Product {
 
 	public void setWeightPerUofM(double weightPerUofM) {
 		this.weightPerUofM = weightPerUofM;
+	}
+
+	public static Set getCatalog() {
+		return catalog;
 	}
 
 }
