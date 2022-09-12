bf9aebabe06bd086620d233659b62ccc80aebfb4
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2022-09-12 16:51:55 +0300

    some changes

--- sandbox/src/main/java/com/r4ndy/SandboxProject.java
+++ sandbox/src/main/java/com/r4ndy/SandboxProject.java
@@ -1,19 +1,15 @@
 package com.r4ndy;
 
-import java.util.Scanner;
 
 public class SandboxProject {
 	public static void main (String[] args){
-		Scanner input = new Scanner(System.in);
-		StringBuilder sbstr = new StringBuilder(input.nextLine());
-		System.out.println(sbstr.toString() + " " + sbstr.capacity());
-		input.close();
 		
+				
 	}
 	
-	public String testerLambda (Testable t) {
-		String s = "";
-		return s;
+	public static int adder (int a, int b) {
+		return a + b;
 	}
+	
 
 }
--- sandbox/src/main/java/com/r4ndy/Testable.java
+++
@@ -1,7 +0,0 @@
-package com.r4ndy;
-
-@FunctionalInterface
-public interface Testable {
-	abstract String lamba (StringBuilder strbld);
-
-}
