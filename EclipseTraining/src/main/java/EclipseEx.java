a350b9625f67806fe540f11ffd2b396f02f4b35f
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2022-09-12 17:22:45 +0300

    Gradle project
    Eclipse import test

---
+++ EclipseTraining/src/main/java/EclipseEx.java
@@ -0,0 +1,7 @@
+public class EclipseEx {
+
+	public static void main(String[] args) {
+		System.out.println("Print some text...");
+	}
+
+}
--- GradleTestProject/lib/src/main/java/com/randy/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.randy;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
-
-}
