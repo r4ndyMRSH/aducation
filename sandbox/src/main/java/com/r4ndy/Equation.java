840ab90e3aa40898ccae8630ff710eba9837883e
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2022-09-29 16:32:58 +0300

    java for testers #3.29
    Optimaze Equation class

--- sandbox/src/main/java/com/r4ndy/Equation.java
+++ sandbox/src/main/java/com/r4ndy/Equation.java
@@ -5,7 +5,7 @@ public class Equation {
 	private double b;
 	private double c;
 
-	//root number
+	// root number
 	private int n;
 
 	public Equation(double a, double b, double c) {
@@ -14,22 +14,28 @@ public class Equation {
 		this.c = c;
 
 		double d = (b * b) - (4 * a * c);
-		if (d > 0) {
-			n = 2;
-		} else {
-			if (d == 0) {
+
+		if (a != 0) {
+			if (d > 0) {
+				n = 2;
+			} else if (d == 0) {
 				n = 1;
 			} else {
 				n = 0;
 			}
+
+		} else if (b != 0) {
+			n = 1;
+		} else if (c != 0) {
+			n = 0;
+		} else {
+			n = -1;
 		}
 	}
 
-	//Get root number
+	// Get root number
 	public int getN() {
 		return n;
 	}
-	
-	
 
 }
