601a1636c1798d18ea5478c4c9942ab9adaea18e
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2022-09-29 17:00:47 +0300

    java for testers #3.31
    Added support for Chrome and Internet Explorer browsers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
@@ -3,19 +3,27 @@ package ru.stqa.pft.adressbook.appmanager;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 public class ApplicationManager {
 
 	private Map<String, Object> vars;
+	private String browser;
 	protected WebDriver driver;
 	private SessionHelper sessionHelper;
 	private GroupHelper groupHelper;
 	private NavigationHelper navigationHelper;
 	private ContactHelper contactHelper;
+
+	public ApplicationManager(String browser) {
+		this.browser = browser;
+		// TODO Auto-generated constructor stub
+	}
 
 	public NavigationHelper getNavigationHelper() {
 		return navigationHelper;
@@ -28,8 +36,15 @@ public class ApplicationManager {
 	JavascriptExecutor js;
 
 	public void init() {
-		System.setProperty("webdriver.gecko.driver", "c:\\tools\\geckodriver\\geckodriver.exe");
-		driver = new FirefoxDriver();
+//		System.setProperty("webdriver.gecko.driver", "c:\\tools\\geckodriver\\geckodriver.exe");
+		if (browser == BrowserType.FIREFOX) {
+			driver = new FirefoxDriver();
+		} else if (browser == BrowserType.CHROME) {
+			driver = new ChromeDriver();
+		} else if (browser == BrowserType.IE){
+			driver = new InternetExplorerDriver();
+		}
+		
 		js = (JavascriptExecutor) driver;
 		vars = new HashMap<String, Object>();
 		sessionHelper = new SessionHelper(driver);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -2,13 +2,14 @@ package ru.stqa.pft.adressbook.tests;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.remote.BrowserType;
 
 import ru.stqa.pft.adressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 	
 	//delegation
-	protected final ApplicationManager app = new ApplicationManager();
+	protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
 	@Before
 	public void setUp() {
